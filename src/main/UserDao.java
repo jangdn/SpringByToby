8ee3420b26ac767da7e2c5c3e6bb3abdaa1f70d1
안 장우 <ajw0409@MIDASIT.local>
2019-10-17 17:42:08 +0900

    3.6 2 queryForInt : query문에 해당하는 길이 만큼 가져오는 메소드인데  왜 인지 안되서 다른 방법으로 적용시켜봤다. 아래 주석들은 하나하나씩 적용해본 방법 참고하도록

--- src/main/UserDao.java
+++ src/main/UserDao.java
@@ -1,8 +1,10 @@
 package main;
 
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.ResultSetExtractor;
 
 import javax.sql.DataSource;
 import java.sql.*;
@@ -19,7 +21,7 @@ public class UserDao {
         this.dataSource = dataSource;
     }
 
-    public void add(final User user) throws SQLException {
+    public void add(final User user){
         this.jdbcTemplate.update("insert into users(id, name, password) values(?,?,?)", user.getId(), user.getName(), user.getPassword());
     }
 
@@ -49,7 +51,7 @@ public class UserDao {
         return user;
     }
 
-    public void deleteAll() throws SQLException {
+    public void deleteAll(){
 //        this.jdbcTemplate.update(new PreparedStatementCreator() {
 //            @Override
 //            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
@@ -59,19 +61,20 @@ public class UserDao {
         this.jdbcTemplate.update("delete from users");
     }
 
-    public int getCount() throws SQLException {
-        Connection c = dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement("SELECT  count(*) from users");
-
-        ResultSet rs = ps.executeQuery();
-        rs.next();
-        int count = rs.getInt(1);
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        return count;
+    public int getCount() {
+        return this.jdbcTemplate.queryForObject("select count(*) from users", Integer.class);
+//        return this.jdbcTemplate.query(new PreparedStatementCreator() {
+//            @Override
+//            public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+//                return connection.prepareStatement("select count(*) from users");
+//            }
+//        }, new ResultSetExtractor<Integer>() {
+//            @Override
+//            public Integer extractData(ResultSet resultSet) throws SQLException, DataAccessException {
+//                resultSet.next();
+//                return resultSet.getInt(1);
+//            }
+//        });
     }
 
 }
